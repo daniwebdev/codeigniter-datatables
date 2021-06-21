89c3a158887f28e8c44eaa98705135be22997d1e
Muhamad Yusup Hamdani <38590276+daniwebdev@users.noreply.github.com>
2021-06-21 17:04:53 +0700

    Update Datatables.php

--- Datatables.php
+++ Datatables.php
@@ -319,7 +319,7 @@
       $mColArray = $this->ci->input->post('columns');
       $sWhere = '';
       $search = $this->ci->input->post('search');
-      $sSearch = $this->ci->db->escape_like_str(trim($search['value']));
+      $sSearch = $this->ci->db->escape_like_str(trim($search['value'] ?? ''));
       $columns = array_values(array_diff($this->columns, $this->unset_columns));
       if($sSearch != '')
         for($i = 0; $i < count($mColArray); $i++)
@@ -472,7 +472,7 @@
     private function check_cType()
     {
       $column = $this->ci->input->post('columns');
-      if(is_numeric($column[0]['data']))
+      if(is_numeric($column[0]['data'] ?? null))
         return FALSE;
       else
         return TRUE;
